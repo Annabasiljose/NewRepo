61f1787a18f80164d5715f27e4a4c8c8dd084fda
Annabasiljose <annabasiljose1@gmail.com>
2022-09-13 11:17:23 +0200

    Namespace commit

--- R/function_add.R
+++ R/function_add.R
@@ -1,3 +1,8 @@
+#' Calculate the area of a circle given its radius
+#' @param a First integer
+#' @param b Second param
+#' @export
+
 add_two_numbers <- function(a, b){
   sum_of_numbers <- a + b
   return(sum_of_numbers)
